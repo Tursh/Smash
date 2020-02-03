6a279050a1e5a0eb714944ff84da23bc9f7fadfa
tursh <raphaeltremblay01@gmail.com>
2020-02-03 14:03:33 -0500

    Custom collosion system done

--- Assets/Scripts/PlayerPhysics.cs
+++ Assets/Scripts/PlayerPhysics.cs
@@ -5,112 +5,138 @@ using UnityEditor.SceneManagement;
 using UnityEditor.UIElements;
 using UnityEngine;
 
-public enum Direction { LEFT, RIGHT, UP, DOWN }
+public enum Direction
+{
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+}
 
 public class PlayerPhysics : MonoBehaviour
 {
-	[SerializeField]
-	private float mvSpeed = 0.5f, airResistance = 0.9f, gravity = -0.1f;
-	public Vector2 velocity;
-	
-	BoxCollider2D bc;
+    [SerializeField] private float mvSpeed = 0.5f, airResistance = 0.9f, gravity = -0.1f, jumpCooldown = 0.25f;
+    public Vector2 velocity;
+
+    BoxCollider2D bc;
     Rigidbody2D rb;
 
-    private bool onGround = false;
-    
+    public bool onGround { get; private set; } = false;
+
     private int playerLayer = 9, stateLayer = 8;
-    
-	private void Start()
-	{
-		bc = GetComponent<BoxCollider2D>();
+
+    private void Start()
+    {
+        bc = GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
-	}
+    }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-	    
-	    //TODO: Do damage depending on the velocity
-	    
-	    if (other.contacts[0].normal.y != 0)
-		    velocity.y = -gravity;
-	    else
-		    velocity.x = 0;
+        //TODO: Do damage depending on the velocity
 
-	    if (other.collider.gameObject.layer == stateLayer)
-	    {
-		    Vector2 position = transform.position;
-		    position.y = (other.collider.bounds.max + bc.bounds.extents).y;
-		    transform.position = position;
-	    }
-    }
-
-
-    private void OnCollisionStay2D(Collision2D other)
-	{
-        //transform.Translate(bc.Distance(collision.collider).normal * bc.Distance(collision.collider).distance);
-        //Debug.Log(bc.Distance(collision.collider).normal * bc.Distance(collision.collider).distance);
-        
-        if (other.collider.gameObject.layer == stateLayer && other.contacts[0].normal == new Vector2(0, 1))
+        if (other.collider.gameObject.layer == stateLayer)
         {
-	        if(!onGround)
-		        velocity.y = 0;
-	        onGround = true;
+            Vector3 normal = other.contacts[0].normal;
+            
+            if (normal == new Vector3(0, 1, 0))
+            {
+                onGround = true;
+                transform.SetParent(GameObject.Find("Stage").transform);
+            }
+
+            if (normal.y != 0)
+            {
+                Vector2 position = transform.position;
+
+                position.y = (onGround ? other.collider.bounds.max : other.collider.bounds.min).y +
+                             bc.bounds.extents.y * normal.y;
+                transform.position = position;
+                velocity.y = 0;
+            }
+            else
+            {
+                Vector2 position = transform.position;
+                position.x = (normal.x > 0 ? other.collider.bounds.max : other.collider.bounds.min).x +
+                             (bc.bounds.extents.x + 0.03f) * normal.x;
+                transform.position = position;
+                velocity.x = 0;
+            }
+
         }
-	}
+    }
 
     private void OnCollisionExit2D(Collision2D other)
     {
-	    if (other.collider.gameObject.layer == stateLayer)
-		    onGround = false;
+        if (other.collider.gameObject.layer == stateLayer)
+        {
+            onGround = false;
+            transform.SetParent(null);
+        }
     }
 
-    public void Update()
-	{
-		velocity *= airResistance;
-		if(!onGround)
-		velocity.y += gravity;
-		transform.Translate(velocity);
-	}
+    private bool first = true;
 
-	public void Move(Direction direction)
-	{
-		switch (direction)
-		{
-			case Direction.LEFT:
-				velocity += Vector2.left * mvSpeed;
-				break;
-			case Direction.RIGHT:
-				velocity += Vector2.right * mvSpeed;
-				break;
-			case Direction.UP:
+    public void FixedUpdate()
+    {
+        velocity *= airResistance;
 
-				break;
-			case Direction.DOWN:
-				break;
-		}
-	}
-	public void StartMovement(Direction direction)
-	{
-		switch (direction)
-		{
-			case Direction.LEFT:
-				velocity += Vector2.left * mvSpeed * 10;
-				break;
-			case Direction.RIGHT:
-				velocity += Vector2.right * mvSpeed * 10;
-				break;
-			case Direction.UP:
+        if (!onGround)
+            velocity.y += gravity;
 
-				break;
-			case Direction.DOWN:
-				velocity += Vector2.down * mvSpeed * 10;
-				break;
-		}
-	}
+        transform.Translate(velocity);
 
-	public void Jump()
-	{
-		
-		velocity.y = 1;
-	}
+        if (onGround && first)
+        {
+            first = false;
+        }
+    }
+
+    public void Move(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.LEFT:
+                velocity += Vector2.left * mvSpeed;
+                break;
+            case Direction.RIGHT:
+                velocity += Vector2.right * mvSpeed;
+                break;
+            case Direction.UP:
+
+                break;
+            case Direction.DOWN:
+                break;
+        }
+    }
+
+    public void StartMovement(Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.LEFT:
+                velocity += Vector2.left * mvSpeed * 10;
+                break;
+            case Direction.RIGHT:
+                velocity += Vector2.right * mvSpeed * 10;
+                break;
+            case Direction.UP:
+                GameObject.Find("Stage").transform.Translate(Vector3.up * 0.1f);
+                break;
+            case Direction.DOWN:
+                velocity += Vector2.down * mvSpeed * 10;
+                break;
+        }
+    }
+
+    private float lastJump;
+
+    public void Jump()
+    {
+        if (Time.time - lastJump >= jumpCooldown)
+        {
+            velocity.y = 1;
+            lastJump = Time.time;
+        }
+    }
 }
