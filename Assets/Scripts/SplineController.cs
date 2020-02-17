dc5962c614ea3b257fab14f288dfc4b35036389b
tursh <raphaeltremblay01@gmail.com>
2020-02-17 15:11:52 -0500

    The spline don't completely finish where they started, but they go from on border to the other, now

--- Assets/Scripts/SplineController.cs
+++ Assets/Scripts/SplineController.cs
@@ -12,6 +12,8 @@ public class SplineController : MonoBehaviour
     private Bounds platformBounds;
     private Vector2[] splinePoints;
 
+    [SerializeField] private uint WantedPointCount = 25, Precision = 50;
+
     public void Start()
     {
         Camera cam = Camera.main;
@@ -19,7 +21,7 @@ public class SplineController : MonoBehaviour
         EndMapPosition = cam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 12));
         MapSize = EndMapPosition - StartMapPosition;
 
-        splinePoints = GenerateSpline(25, 25);
+        splinePoints = GenerateSpline(WantedPointCount, Precision);
     }
 
     private int time = 0;
@@ -27,56 +29,79 @@ public class SplineController : MonoBehaviour
     private void Update()
     {
         transform.position = splinePoints[(time++) % (25 * 25)];
+        if (time % 10 == 0)
+            Instantiate(GameObject.CreatePrimitive(PrimitiveType.Sphere), transform.position, Quaternion.identity);
     }
 
     Vector2 getRandomPointInMap(Vector2 centerPoint, float radius)
     {
         Vector2 point = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
-        point = centerPoint + point.normalized * radius - StartMapPosition;
-        point.x = (point.x % MapSize.x) + StartMapPosition.x;
-        point.y = (point.y % MapSize.y) + StartMapPosition.y;
+        point = centerPoint + point.normalized * radius;
         return point;
     }
 
     Vector2[] GenerateSpline(uint pointCount, uint precision)
     {
-        Vector2[] points = new Vector2[pointCount * precision];
-        Vector2[] keyPoints = new Vector2[pointCount];
+        List<Vector2> keyPoints = new List<Vector2>();
 
         //Get the first point
-        keyPoints[0] = new Vector2(Random.Range(StartMapPosition.x, EndMapPosition.x),
-            Random.Range(StartMapPosition.y, EndMapPosition.y));
+        keyPoints.Add(new Vector2(Random.Range(StartMapPosition.x, EndMapPosition.x),
+            Random.Range(StartMapPosition.y, EndMapPosition.y)));
 
-        //Get the rest of the key points
+        //Generate the rest of the points
         for (int i = 1; i < pointCount; ++i)
         {
-            keyPoints[i] = getRandomPointInMap(keyPoints[i - 1], 5);
+            keyPoints.Add(getRandomPointInMap(keyPoints[i - 1], 5));
             if (i > 1)
             {
-                float angleBetween = Vector2.Angle(keyPoints[i - 1], keyPoints[i]) +
+                float angleBetween = Vector2.Angle(keyPoints[i - 1], keyPoints[i]) -
                                      Vector2.Angle(keyPoints[i - 2], keyPoints[i - 1]);
-                if (angleBetween > 45.0f)
+                if (angleBetween > 60.0f)
                     --i;
             }
         }
 
-        float[] x = new float[pointCount];
-        float[] y = new float[pointCount];
+        int extraPointCount = -1;
+        Vector2Int offset = Vector2Int.FloorToInt((keyPoints[(int) pointCount - 1] - StartMapPosition) / MapSize);
+        Vector2 startPointWithOffset = keyPoints[0] + offset * MapSize;
+        Vector2 Distance;
 
-        for (int i = 0; i < pointCount; ++i)
+        do
+        {
+            Vector2 lastPoint = keyPoints[(int) (pointCount + extraPointCount)];
+            Distance = startPointWithOffset - lastPoint;
+            keyPoints.Add(lastPoint + Distance.normalized * Math.Min(Distance.magnitude, 5.0f));
+            ++extraPointCount;
+        } while (Distance.magnitude < 1.0f);
+
+        //Generate the spline
+        float[] x = new float[keyPoints.Count];
+        float[] y = new float[keyPoints.Count];
+
+        for (int i = 0; i < keyPoints.Count; ++i)
         {
             x[i] = keyPoints[i].x;
             y[i] = keyPoints[i].y;
         }
 
-        float[] xs = new float[pointCount * precision];
-        float[] ys = new float[pointCount * precision];
+        float[] xs = new float[keyPoints.Count * precision];
+        float[] ys = new float[keyPoints.Count * precision];
 
-        CubicSpline.FitParametric(x, y, (int) (pointCount * precision), out xs, out ys);
+        CubicSpline.FitParametric(x, y, (int) (keyPoints.Count * precision), out xs, out ys);
+
+        Vector2[] points = new Vector2[pointCount * precision];
 
         for (int i = 0; i < pointCount * precision; ++i)
         {
             points[i] = new Vector2(xs[i], ys[i]);
+            if (points[i].x < StartMapPosition.x)
+                points[i].x += MapSize.x;
+            else if (points[i].x > EndMapPosition.x)
+                points[i].x -= MapSize.x;
+            if (points[i].y < StartMapPosition.y)
+                points[i].y += MapSize.y;
+            else if (points[i].y > EndMapPosition.y)
+                points[i].y -= MapSize.y;
         }
 
         return points;
